4f355d3c82ffe7ecb1269dee87b6b2ada11ecbe9
Mickael Lanfranchi <58193062+cecceccu@users.noreply.github.com>
2021-11-19 10:59:03 +0100

    First commit

---
+++ cityweather/cityweather/src/main/java/com/weather/lanfranchi/cityweather/CityweatherApplication.java
@@ -0,0 +1,13 @@
+package com.weather.lanfranchi.cityweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityweatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityweatherApplication.class, args);
+	}
+
+}
---
+++ cityweather/cityweather/src/test/java/com/weather/lanfranchi/cityweather/CityweatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weather.lanfranchi.cityweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityweatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
